f5e7e8b19455f541b9d84af5c66eceb6e129e8bd
Zachary Lentz <zlentz@slac.stanford.edu>
2020-04-17 16:48:56 -0700

    WIP: first draft at lineid remover hook

---
+++ pre_commit_hooks/twincat_lineids_remover.py
@@ -0,0 +1,30 @@
+import argparse
+
+
+def fix_file(filename):
+    with open(filename, 'r') as fd:
+        original_lines = fd.readlines()
+    new_lines = []
+    for line in original_lines:
+        if '<LineId' not in line and 'LineIds>' not in line:
+            new_lines.append(line)
+    with open(filename, 'w') as fd:
+        fd.write(''.join(new_lines))
+
+
+def main(args=None):
+    if args is None:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('filenames', nargs='*')
+        args = parser.parse_args()
+    try:
+        for filename in args.filenames:
+            fix_file(filename)
+        return 0
+    except Exception as exc:
+        print(exc)
+        return 1
+
+
+if __name__ == "__main__":
+    exit(main())
