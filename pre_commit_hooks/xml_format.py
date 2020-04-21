37a992fa53360d4a9e99347e20ffd7f588ed17ed
Zachary Lentz <zlentz@slac.stanford.edu>
2020-04-21 11:25:01 -0700

    ENH: preserve line endings and add comments

--- pre_commit_hooks/xml_format.py
+++ pre_commit_hooks/xml_format.py
@@ -4,15 +4,23 @@ from lxml import etree
 
 
 def fix_file(filename):
+    # lxml throws encoding errors unless we work in binary mode
     with open(filename, 'rb') as fd:
         original_xml = fd.read()
 
+    # lxml is the easiest cross-platform way to do this using pre-commit
+    # xmllint is cross-platform but pre-commit does not help us set it up
     xml_parser = etree.XMLParser(remove_blank_text=True)
     parse_tree = etree.XML(original_xml, parser=xml_parser).getroottree()
     new_xml = etree.tostring(parse_tree,
                              pretty_print=True,
                              xml_declaration=True,
                              encoding=parse_tree.docinfo.encoding)
+
+    # lxml does not preserve line endings, so we must do it ourselves.
+    # lxml always outputs with unix line endings (LF)
+    if b'\r\n' in original_xml:
+        new_xml = new_xml.replace(b'\n', b'\r\n')
 
     if new_xml != original_xml:
         print(f'Fixing {filename}')
