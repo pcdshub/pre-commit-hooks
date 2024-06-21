468b9c80600b9864968b377b4cddae174a3e9f49
Roald Ruiter <roald.ruiter@inspire.ch>
2024-06-21 16:38:02 +0200

    use fixtures

--- pre_commit_hooks/tests/test_check_twincat_versions.py
+++ pre_commit_hooks/tests/test_check_twincat_versions.py
@@ -1,14 +1,22 @@
 from importlib.resources import files
 
+import pytest
+
 import pre_commit_hooks.tests.data as test_data
 from pre_commit_hooks.check_twincat_versions import tc_version_pinned
 
 
-def test_pinned_version():
-    tsproj_filename = files(test_data).joinpath("pinned-version-3.1.4024.44.tsproj")
-    assert tc_version_pinned(tsproj_filename)
+@pytest.fixture
+def pinned_4024_44():
+    return files(test_data).joinpath("pinned-version-3.1.4024.44.tsproj")
+
+@pytest.fixture
+def not_pinned_4024_22():
+    return files(test_data).joinpath("version-3.1.4024.22.tsproj")
+
+def test_pinned_version(pinned_4024_44):
+    assert tc_version_pinned(pinned_4024_44)
 
 
-def test_absent_pinned_version():
-    tsproj_filename = files(test_data).joinpath("version-3.1.4024.22.tsproj")
-    assert not tc_version_pinned(tsproj_filename)
+def test_absent_pinned_version(not_pinned_4024_22):
+    assert not tc_version_pinned(not_pinned_4024_22)
