9d7ed399af62b75d6cfce5361b48631ac7f9a91f
Roald Ruiter <roald.ruiter@inspire.ch>
2024-06-21 17:14:51 +0200

    add fix version method

--- pre_commit_hooks/check_twincat_versions.py
+++ pre_commit_hooks/check_twincat_versions.py
@@ -1,17 +1,21 @@
+import re
 import xml.etree.ElementTree as ET
-from importlib.abc import Traversable
-from typing import Union
 
 
-def tc_version_pinned(filename: Union[Traversable, str]) -> bool:
-    tree = ET.parse(filename)
-    root = tree.getroot()
+def tc_version_pinned(xml_content: str) -> bool:
+    root = ET.fromstring(xml_content)
 
     return 'TcVersionFixed' in root.attrib
 
 
-def get_tc_version(filename: Union[Traversable, str]) -> str:
-    tree = ET.parse(filename)
-    root = tree.getroot()
+def get_tc_version(xml_content: str) -> str:
+    root = ET.fromstring(xml_content)
 
     return root.attrib.get('TcVersion')
+
+
+def fix_tc_version(xml_content: str, new_version: str) -> str:
+    pattern = r'(TcVersion=")([^"]*)(")'
+    new_xml_content = re.sub(pattern, r'\g<1>' + new_version + r'\g<3>', xml_content)
+
+    return new_xml_content
--- pre_commit_hooks/tests/test_check_twincat_versions.py
+++ pre_commit_hooks/tests/test_check_twincat_versions.py
@@ -2,23 +2,24 @@ from importlib.resources import files
 
 import pre_commit_hooks.tests.data as test_data
 import pytest
-from pre_commit_hooks.check_twincat_versions import (get_tc_version,
+from pre_commit_hooks.check_twincat_versions import (fix_tc_version,
+                                                     get_tc_version,
                                                      tc_version_pinned)
 
 
 @pytest.fixture
 def pinned_4024_44():
-    return files(test_data).joinpath("pinned-version-3.1.4024.44.tsproj")
+    return files(test_data).joinpath("pinned-version-3.1.4024.44.tsproj").read_text()
 
 
 @pytest.fixture
 def not_pinned_4024_22():
-    return files(test_data).joinpath("version-3.1.4024.22.tsproj")
+    return files(test_data).joinpath("version-3.1.4024.22.tsproj").read_text()
 
 
 @pytest.fixture
 def not_pinned_4024_55():
-    return files(test_data).joinpath("version-3.1.4024.55.tsproj")
+    return files(test_data).joinpath("version-3.1.4024.55.tsproj").read_text()
 
 
 def test_pinned_version(pinned_4024_44):
@@ -33,3 +34,11 @@ def test_get_tc_version(pinned_4024_44, not_pinned_4024_22, not_pinned_4024_55):
     assert get_tc_version(pinned_4024_44) == "3.1.4024.44"
     assert get_tc_version(not_pinned_4024_22) == "3.1.4024.22"
     assert get_tc_version(not_pinned_4024_55) == "3.1.4024.55"
+
+
+def test_fix_tc_version(pinned_4024_44, not_pinned_4024_22):
+    changed_version1 = fix_tc_version(pinned_4024_44, "3.1.4024.55")
+    assert get_tc_version(changed_version1) == "3.1.4024.55"
+
+    changed_version2 = fix_tc_version(not_pinned_4024_22, "3.1.4024.55")
+    assert get_tc_version(changed_version2) == "3.1.4024.55"
