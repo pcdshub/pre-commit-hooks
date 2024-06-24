a6d7e3a27a7728a858f11ef7b050b3c728f08061
Roald Ruiter <roald.ruiter@inspire.ch>
2024-06-24 11:36:32 +0200

    add and run ruff on all files

--- pre_commit_hooks/minimize_id_changes.py
+++ pre_commit_hooks/minimize_id_changes.py
@@ -12,18 +12,19 @@ def minimize_id_changes_checked(filename: Union[Traversable, str]) -> None:
     tree = ET.parse(filename)
     root = tree.getroot()
 
-    combine_ids_element = root.find('.//{*}CombineIds')
+    combine_ids_element = root.find(".//{*}CombineIds")
 
-    if combine_ids_element is None or combine_ids_element.text.lower() == 'false':
+    if combine_ids_element is None or combine_ids_element.text.lower() == "false":
         raise PreCommitException(
             f"Minimize id changes not checked in {filename}. "
-            f"To enable this go to Project settings > Common and select 'Minimize Id changes in TwinCAT files.'")
+            f"To enable this go to Project settings > Common and select 'Minimize Id changes in TwinCAT files.'"
+        )
 
 
 def main(args=None):
     if args is None:
         parser = argparse.ArgumentParser()
-        parser.add_argument('filenames', nargs='*')
+        parser.add_argument("filenames", nargs="*")
         args = parser.parse_args()
     try:
         for filename in args.filenames:
--- pre_commit_hooks/twincat_lineids_remover.py
+++ pre_commit_hooks/twincat_lineids_remover.py
@@ -4,27 +4,27 @@ import argparse
 
 
 def fix_file(filename):
-    with open(filename, 'r') as fd:
+    with open(filename, "r") as fd:
         original_lines = fd.readlines()
     new_lines = []
     changed = False
 
     for line in original_lines:
-        if '<LineId' in line or 'LineIds>' in line:
+        if "<LineId" in line or "LineIds>" in line:
             changed = True
         else:
             new_lines.append(line)
 
     if changed:
-        print(f'Fixing {filename}')
-        with open(filename, 'w') as fd:
-            fd.write(''.join(new_lines))
+        print(f"Fixing {filename}")
+        with open(filename, "w") as fd:
+            fd.write("".join(new_lines))
 
 
 def main(args=None):
     if args is None:
         parser = argparse.ArgumentParser()
-        parser.add_argument('filenames', nargs='*')
+        parser.add_argument("filenames", nargs="*")
         args = parser.parse_args()
     try:
         for filename in args.filenames:
