e9b978bddb48950435381312e7edf3c079d29abb
Roald Ruiter <roald.ruiter@inspire.ch>
2024-06-24 10:51:47 +0200

    add cli to check twincat versions

--- pre_commit_hooks/check_twincat_versions.py
+++ pre_commit_hooks/check_twincat_versions.py
@@ -1,5 +1,7 @@
+import argparse
 import re
 import xml.etree.ElementTree as ET
+from exceptions import PreCommitException
 
 
 def tc_version_pinned(xml_content: str) -> bool:
@@ -33,3 +35,40 @@ def fix_pinned_version(xml_content: str, pin_version: bool) -> str:
         new_xml_content = re.sub(version_pattern, r'\g<1> TcVersionFixed="' + new_value + r'"', xml_content)
 
     return new_xml_content
+
+def main(args=None):
+    if args is None:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('filenames', nargs='+', help='List of XML filenames to process')
+        parser.add_argument('--target-version', type=str, help='Target TcVersion to enforce')
+        args = parser.parse_args()
+
+    try:
+        versions = {}
+        for filename in args.filenames:
+            with open(filename, 'r') as file:
+                xml_content = file.read()
+                version = get_tc_version(xml_content)
+                versions[filename] = version
+
+        itemize = "\n -"
+        if args.target_version:
+            mismatched_files = [fname for fname, ver in versions.items() if ver != args.target_version]
+            if mismatched_files:
+                raise PreCommitException(
+                    "The following files are not set to the targeted TwinCAT version "
+                    f"{args.target_version}:{itemize}{itemize.join(mismatched_files)}")
+        else:
+            unique_versions = set(versions.values())
+            if len(unique_versions) > 1:
+                raise PreCommitException(
+                    "Not all files have the same TwinCAT version:"
+                    f"{itemize}" + itemize.join(f"{fname}: {ver}" for fname, ver in versions.items()))
+
+        return 0
+    except Exception as exc:
+        print(exc)
+        return 1
+
+if __name__ == "__main__":
+    exit(main())
