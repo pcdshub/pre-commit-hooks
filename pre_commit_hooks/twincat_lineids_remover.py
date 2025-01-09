ba003206541169b880ac61e98666f63ea19691e7
Zachary Lentz <zlentz@slac.stanford.edu>
2025-01-08 16:05:55 -0800

    ENH/FIX: display failing filename in twincat-lineids-remover, use utf encoding in all non-binary file opens

--- pre_commit_hooks/twincat_lineids_remover.py
+++ pre_commit_hooks/twincat_lineids_remover.py
@@ -4,7 +4,7 @@ import argparse
 
 
 def fix_file(filename):
-    with open(filename, "r") as fd:
+    with open(filename, "r", encoding="utf-8") as fd:
         original_lines = fd.readlines()
     new_lines = []
     changed = False
@@ -17,7 +17,7 @@ def fix_file(filename):
 
     if changed:
         print(f"Fixing {filename}")
-        with open(filename, "w") as fd:
+        with open(filename, "w", encoding="utf-8") as fd:
             fd.write("".join(new_lines))
 
 
@@ -26,11 +26,14 @@ def main(args=None):
         parser = argparse.ArgumentParser()
         parser.add_argument("filenames", nargs="*")
         args = parser.parse_args()
+    filename = None
     try:
         for filename in args.filenames:
             fix_file(filename)
         return 0
     except Exception as exc:
+        if filename is not None:
+            print(f"Error while processing {filename}")
         print(exc)
         return 1
 
